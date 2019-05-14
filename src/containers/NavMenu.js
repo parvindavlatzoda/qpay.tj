4e7bec88daab0236815f07ab90bc33c8e24c4b2a
parvindavlatzoda <pdavlatzoda@bk.ru>
2019-05-14 10:28:21 +0500

    поменяла логотип на qpay

--- src/containers/NavMenu.js
+++ src/containers/NavMenu.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../images/qiwi_logo.png';
-import './NavMenu.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/qpay_logo.svg";
+import "./NavMenu.css";
 
 export default class NavMenu extends React.Component {
   render() {
     return (
       <nav>
-          <Link to={'/'}>
-            <img src={logo} classname="logo" alt="logo" style={{ width: '9.5em'}}/>
-          </Link>
+        <Link to={"/"}>
+          <img
+            src={logo}
+            classname="logo"
+            alt="logo"
+            style={{ width: "6em" }}
+          />
+        </Link>
         <ul>
           <li>
-            <Link to={'/payments'}>Платежи и переводы</Link>
+            <Link to={"/payments"}>Платежи и переводы</Link>
           </li>
           <li>
-            <Link to={'/advertising'}>Реклама</Link>
+            <Link to={"/advertising"}>Реклама</Link>
           </li>
           <li>
-            <Link to={'/'}>Благотворительность</Link>
+            <Link to={"/"}>Благотворительность</Link>
           </li>
           <li>
-            <Link to={'/'}>Пополнение кошелька</Link>
+            <Link to={"/"}>Пополнение кошелька</Link>
           </li>
         </ul>
       </nav>
-    )
+    );
   }
 }
