815930e556ecd57f1322ce073d81667a430e03fa
parvindavlatzoda <pdavlatzoda@bk.ru>
2019-07-03 14:20:34 +0500

    просто

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -3,10 +3,27 @@ import { Link } from "react-router-dom";
 import "./MobileMenu.css";
 
 export default class MobileMenu extends React.Component {
+  state = {
+    openMenu: false
+  };
+
+  СhangeMenu = () => {
+    this.setState({ openMenu: !this.state.openMenu });
+    console.log("change state");
+    // this.setState(state => ({ openMenu: !state.openMenu }));
+    // this.setState(({ menuOpen }) => ({ !menuOpen }));
+    // console.log({ openMenu });
+    // this.state();
+  };
   render() {
     return (
       <div className="burger-menu">
-        <input type="checkbox" id="overlay-input" />
+        <input
+          onChange={this.ChangeMenu}
+          checked={this.state.openMenu}
+          type="checkbox"
+          id="overlay-input"
+        />
         <label for="overlay-input" id="overlay-button">
           <span />
         </label>
@@ -16,7 +33,12 @@ export default class MobileMenu extends React.Component {
               <a href="#">Home</a>
             </li> */}
             <li>
-              <Link to={"/payments"}>Платежи и переводы</Link>
+              <Link
+                // onClick={this.CloseMenu}
+                to={"/payments"}
+              >
+                Платежи и переводы
+              </Link>
             </li>
             {/* <li>
               <Link to={"/advertising"}>Реклама</Link>
